fe59a9f17d989c2b89304a0069cc77ecfff5c86f
tmdgus1237 <tmd1237@gmail.com>
2022-04-07 23:41:40 +0900

    #1 Hello World 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
 
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  <h1> Hello World</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 });
 </script>
